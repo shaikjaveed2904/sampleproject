ff4bd85183b16ebc3f2ae7e15bc4dd6b41b58b5c
shaik <skjaveed2529@gmail.com>
2022-12-14 04:04:37 +0000

    multiplication updated

--- addition.py
+++ addition.py
@@ -3,6 +3,8 @@ y = input("Type another number: ")
 
 sum = int(x) + int(y)
 sub = int(x) - int(y)
+mul = int(x) * int(y)
 print("The sum is: ", sum)
 print("The sub is: ", sub)
+print("The mul is: ", mul)
 
